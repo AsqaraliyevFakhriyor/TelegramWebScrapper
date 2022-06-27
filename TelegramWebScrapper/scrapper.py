46b17a5c69888596918f4a406983c09b18eb595c
Muhammadali-Akbarov <muhammadali17abc@gmail.com>
2022-06-27 18:44:10 +0500

    small changes

--- TelegramWebScrapper/scrapper.py
+++ TelegramWebScrapper/scrapper.py
@@ -1,26 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+
 class TelegramgWebScrapper:
     """Telegram Web scrapper
     You can get informations about telegram posts by using the methods bellow
     (only for telegram channels)"""
 
-    __BASE_URL = "https://t.me/s/"
-    __BS4 = __import__("bs4")
-    __REQUESTS = __import__("requests")
+    __PARSE_MODE = "html.parser"
+    __WIDGET_VIEWS = "tgme_widget_message_views"
+    __WIDGET_MESSAGE = "tgme_widget_message js-widget_message"
+    __NOT_FOUND_MESSAGE = "Make sure that you have correct message id"
 
-
-    def __init__(self, channel: str) -> None:
-        self.channel = channel
-        self.channel_url = self.__BASE_URL + str(channel)
-        self.html_page = self.__REQUESTS.get(self.channel_url)
-        self.soup =self.__BS4.BeautifulSoup(self.html_page.text, "html.parser")
-
+    def __init__(self, base_url: str,  channel: str) -> None:
+        self.channel_url = base_url + channel
+        self.soup = BeautifulSoup(requests.get(
+            self.channel_url).text, self.__PARSE_MODE)
 
     def get_post_views(self, message_id: int) -> str:
-        soup = self.soup.find(class_="tgme_widget_message js-widget_message", attrs={"data-post": "%s/%s" % (self.channel, str(message_id))})
+        kwargs: dict = {
+            "class_": self.__WIDGET_MESSAGE,
+            "attrs": {
+                {"data-post": f"{self.channel_url}/({str(message_id)})"}
+            }
+        }
+        soup = self.soup.find(**kwargs)
         if soup is None:
-            raise self.__BS4.FeatureNotFound("Make sure that you have correct message id")
-        views = soup.find(class_="tgme_widget_message_views")
-        return views.text
+            raise BeautifulSoup.FeatureNotFound(self.__NOT_FOUND_MESSAGE)
 
-    
-        
+        return soup.find(class_=self.__WIDGET_VIEWS).text
